a87049dd272dee2cfc9c3bc5f37771ccdb88418b
Artemis <113257055+Artemis-solomon@users.noreply.github.com>
2023-11-25 20:37:15 -0500

    Update app.py

--- web_interfaces/app.py
+++ web_interfaces/app.py
@@ -1,1 +1,22 @@
+from flask import Flask, render_template, request
+from sql_injection_detector import detect_sql_injection
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/scan', methods=['POST'])
+def scan():
+    input_data = request.form.get('input_data')
+    result = detect_sql_injection(input_data)
+    
+    if result:
+        return render_template('result.html', result='Potential SQL injection detected!')
+    else:
+        return render_template('result.html', result='No SQL injection detected.')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
