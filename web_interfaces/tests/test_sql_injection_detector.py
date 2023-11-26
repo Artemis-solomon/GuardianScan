3992d3985147d7f6bb494e12676c521922b13ce4
Artemis <113257055+Artemis-solomon@users.noreply.github.com>
2023-11-25 20:38:08 -0500

    Update test_sql_injection_detector.py

--- web_interfaces/tests/test_sql_injection_detector.py
+++ web_interfaces/tests/test_sql_injection_detector.py
@@ -1,1 +1,16 @@
+import unittest
+from sql_injection_detector import detect_sql_injection
+
+class TestSQLInjectionDetector(unittest.TestCase):
+    def test_detect_sql_injection(self):
+        # Test for potential SQL injection
+        sql_injection_input = "SELECT * FROM users WHERE username = 'admin' OR '1' = '1';"
+        self.assertTrue(detect_sql_injection(sql_injection_input))
+
+        # Test for non-SQL injection
+        non_sql_injection_input = "SELECT * FROM users WHERE username = 'john.doe';"
+        self.assertFalse(detect_sql_injection(non_sql_injection_input))
+
+if __name__ == '__main__':
+    unittest.main()
 
