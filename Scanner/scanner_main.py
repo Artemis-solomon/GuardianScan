bee2b636960dafd9af4f0326805cd0c0bb4c10cd
Artemis <113257055+Artemis-solomon@users.noreply.github.com>
2023-11-25 20:35:46 -0500

    Update scanner_main.py

--- Scanner/scanner_main.py
+++ Scanner/scanner_main.py
@@ -1,1 +1,11 @@
+# scanner_main.py
+
+from sql_injection_detector import detect_sql_injection
+
+# Example usage
+user_input = input("Enter data to scan for SQL injection: ")
+if detect_sql_injection(user_input):
+    print("Potential SQL injection detected!")
+else:
+    print("No SQL injection detected.")
 
